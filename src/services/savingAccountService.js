49bf38588694f452ee70b5396256d794a7ffe77f
Zach Haun <zachhaun@ZachHaun-Mac.local>
2019-05-02 14:26:40 -0400

    Updated CIT APY rate

--- src/services/savingAccountService.js
+++ src/services/savingAccountService.js
@@ -1,47 +1,47 @@
 const savings = [
-    {
-        _id: "SA01",
-        title: "CIT Savings Builder",
-        siteLink: "https://www.cit.com/cit-bank/tiered-savings-account-savings-builder-value/",
-        requirements: "$100 Monthly Deposit / $25K Balance",
-        bonus: 0,
-        apy: "2.45%"
-    },
-    {
-        _id: "SA02",
-        title: "Marcus By Goldman Sachs",
-        siteLink: "https://www.marcus.com/us/en",
-        requirements: "$1 to Open",
-        bonus: 0,
-        apy: "2.25%"
-    },
-    {
-        _id: "SA03",
-        title: "Ally Online Savings Account",
-        siteLink: "https://www.ally.com/bank/online-savings-account/",
-        requirements: "None",
-        bonus: 0,
-        apy: "2.20%"
-    },
-    {
-        _id: "SA04",
-        title: "Discover Online Savings Account",
-        siteLink: "https://www.discover.com/online-banking/savings-account/",
-        requirements: "$25K Opening Balance to get Bonus",
-        bonus: 200,
-        apy: "2.10%"
-    },
-    {
-        _id: "SA05",
-        title: "American Express Personal Savings",
-        siteLink: "https://www.americanexpress.com/personalsavings/home.html",
-        requirements: "None",
-        bonus: 0,
-        apy: "2.10%"
-    }
-
+  {
+    _id: "SA01",
+    title: "CIT Savings Builder",
+    siteLink:
+      "https://www.cit.com/cit-bank/tiered-savings-account-savings-builder-value/",
+    requirements: "$100 Monthly Deposit / $25K Balance",
+    bonus: 0,
+    apy: "2.40%"
+  },
+  {
+    _id: "SA02",
+    title: "Marcus By Goldman Sachs",
+    siteLink: "https://www.marcus.com/us/en",
+    requirements: "$1 to Open",
+    bonus: 0,
+    apy: "2.25%"
+  },
+  {
+    _id: "SA03",
+    title: "Ally Online Savings Account",
+    siteLink: "https://www.ally.com/bank/online-savings-account/",
+    requirements: "None",
+    bonus: 0,
+    apy: "2.20%"
+  },
+  {
+    _id: "SA04",
+    title: "Discover Online Savings Account",
+    siteLink: "https://www.discover.com/online-banking/savings-account/",
+    requirements: "$25K Opening Balance to get Bonus",
+    bonus: 200,
+    apy: "2.10%"
+  },
+  {
+    _id: "SA05",
+    title: "American Express Personal Savings",
+    siteLink: "https://www.americanexpress.com/personalsavings/home.html",
+    requirements: "None",
+    bonus: 0,
+    apy: "2.10%"
+  }
 ];
 
 export function getSavings() {
-    return savings;
+  return savings;
 }
